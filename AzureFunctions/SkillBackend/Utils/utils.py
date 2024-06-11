fe6bdf40b61feebca5cb5564a6249f50519212f6
davideferrari95 <davideferr@unimore.it>
2024-06-11 15:29:36 +0200

    Added utils

--- AzureFunctions/SkillBackend/Utils/utils.py
+++ AzureFunctions/SkillBackend/Utils/utils.py
@@ -5,8 +5,7 @@ def is_api_request(handler_input, api_name):
 
     """ Helper method to check if the incoming request is an API request. """
 
-    try:
-        return ask_utils.request_util.get_request_type(handler_input) == 'Dialog.API.Invoked' and handler_input.request_envelope.request.api_request.name == api_name
+    try: return ask_utils.request_util.get_request_type(handler_input) == 'Dialog.API.Invoked' and handler_input.request_envelope.request.api_request.name == api_name
     except Exception as ex:
         logging.error(ex)
         return False
@@ -15,10 +14,18 @@ def get_api_arguments(handler_input):
 
     """Helper method to get API arguments from the request envelope."""
 
-    try:
-        return handler_input.request_envelope.request.api_request.arguments
+    try: return handler_input.request_envelope.request.api_request.arguments
     except Exception as ex:
-        logging.error(ex)
+        logging.error('Error occurred: ', ex)
+        return False
+
+def get_slots(handler_input):
+
+    """ Helper method to get slots from the request envelope. """
+
+    try: return handler_input.request_envelope.request.api_request.slots
+    except Exception as ex:
+        logging.error('Error occurred: ', ex)
         return False
 
 def setup_logging():
